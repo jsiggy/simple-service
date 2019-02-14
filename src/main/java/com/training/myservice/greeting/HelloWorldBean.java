ba7f564eea359c5c2aaf14f8abfb05c3ffc68775
Sigler, John <John.Sigler@schwab.com>
2019-02-14 16:49:13 -0600

    GET failed - add getter so client can display value

--- src/main/java/com/training/myservice/greeting/HelloWorldBean.java
+++ src/main/java/com/training/myservice/greeting/HelloWorldBean.java
@@ -1,10 +1,12 @@
 package com.training.myservice.greeting;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.ToString;
 
 @EqualsAndHashCode @ToString
 public class HelloWorldBean {
+    @Getter
     private String message;
 
     public HelloWorldBean(String message) {
