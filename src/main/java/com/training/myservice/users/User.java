2c786b9a3d762cc466c71eebd65f0e6caa9cd7f0
Sigler, John <John.Sigler@schwab.com>
2019-02-15 16:22:28 -0600

    Add validation for User (but no information sent to consumer)

--- src/main/java/com/training/myservice/users/User.java
+++ src/main/java/com/training/myservice/users/User.java
@@ -2,12 +2,16 @@ package com.training.myservice.users;
 
 import lombok.*;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @NoArgsConstructor @Getter @Setter @EqualsAndHashCode @ToString @Builder
 public class User {
     private Integer id;
+    @Size(min = 2)
     private String name;
+    @Past
     private Date birthdate;
 
     public User(String name, Date birthdate) {
--- src/main/java/com/training/myservice/users/UserController.java
+++ src/main/java/com/training/myservice/users/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -28,7 +29,7 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         final User newUser = userService.save(user);
 
         final URI location = ServletUriComponentsBuilder
