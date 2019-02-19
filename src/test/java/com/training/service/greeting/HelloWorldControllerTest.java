a447ce24203aa7cd05a95c2a5ce8f5edf5c61d11
Sigler, John <John.Sigler@schwab.com>
2019-02-19 09:30:55 -0600

    Small cleanup

--- src/test/java/com/training/service/greeting/HelloWorldControllerTest.java
+++ src/test/java/com/training/service/greeting/HelloWorldControllerTest.java
@@ -7,17 +7,17 @@ import static org.junit.Assert.assertEquals;
 public class HelloWorldControllerTest {
 
     @Test
-    public void helloWorldReturnsHelloWorldString() {
+    public void shouldReturnHelloWorld() {
         assertEquals("Hello World", new HelloWorldController().helloWorld());
     }
 
     @Test
-    public void helloWorldBeanReturnHelloWorldBean() {
+    public void shouldReturnHelloWorldBeansMessage() {
         assertEquals(new HelloWorldBean("Hi, John"), new HelloWorldController().helloWorldBean());
     }
 
     @Test
-    public void helloWorldNameReturnsTheName() {
+    public void shouldReturnPathVariableName() {
         assertEquals(new HelloWorldBean("Hello John"), new HelloWorldController().helloWorldPathVariableExample("John"));
     }
 }
